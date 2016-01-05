559916e28b4d33994afa4057979e45415e332fda
Hye Kyong Kweon <hkweon@umich.edu>
2016-01-05 13:05:43 -0500

    R program editted

---
+++ variables.R
@@ -0,0 +1,29 @@
+#This is the document where I learn introductory R.
+dist10<-rnorm(10) 
+x <- 1:5
+2^x
+x^2
+x <- y
+y <- 4
+x+y
+
+x <-1:10
+
+mass <- 47.5
+age <-122
+mass <-mass *2.3
+age <- age -20
+mass > age #compare mass to age
+age <-122
+mass <-100
+age <- age-25
+age > mass #compare age to mass'
+rm (age,mass) #clean the workin genvironment
+xx <- c(1.7, "a")
+as.numeric(xx)
+xx
+str(xx)
+
+
+
+
